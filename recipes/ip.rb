3d8029c024d61866d785652068b946323b346b6b
adachish <adachish@amazon.com>
2018-05-01 02:47:11 +0000

    10th

--- recipes/ip.rb
+++ recipes/ip.rb
@@ -1,7 +1,14 @@
-Chef::Log.info("Start of sleep")
-sleep(10)
-Chef::Log.info("End of sleep")
+Chef::Log.info("Start of loop")
 
-instance = search("aws_opsworks_instance", "self:false").first
+for i in 1..60 do
+  instance = search("aws_opsworks_instance", "self:false").first
+  if instance['private_ip'] then
+    break
+  end
+  sleep(5)
+end
+
+Chef::Log.info("End of loop")
+
 Chef::Log.info("********** remote node ip address is '#{instance['private_ip']}' **********")
 
