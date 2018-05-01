c0ab335f7dc1757b9c902afa40534d2a1e3f672d
adachish <adachish@amazon.com>
2018-05-01 00:22:06 +0000

    7th

--- recipes/ip.rb
+++ recipes/ip.rb
@@ -1,7 +1,4 @@
-def remote_instance_ip
-  instance = search("aws_opsworks_instance", "self:false").first
-  instance['private_ip']
-end
+instance = search("aws_opsworks_instance", "self:false").first
 
-Chef::Log.info("********** remote node ip address is '#{remote_instance_ip}' **********")
+Chef::Log.info("********** remote node ip address is '#{instance['private_ip']}' **********")
 
